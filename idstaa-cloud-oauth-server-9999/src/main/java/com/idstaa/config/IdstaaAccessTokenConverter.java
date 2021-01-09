a106769cde5e378145490591259c661605114778
xjdm <1121245860@qq.com>
2021-01-09 19:11:46 +0800

    1、nacos 2、sentinel

--- idstaa-cloud-oauth-server-9999/src/main/java/com/idstaa/config/IdstaaAccessTokenConverter.java
+++ idstaa-cloud-oauth-server-9999/src/main/java/com/idstaa/config/IdstaaAccessTokenConverter.java
@@ -1,6 +1,5 @@
 package com.idstaa.config;
 
-import io.undertow.servlet.attribute.ServletRequestAttribute;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
